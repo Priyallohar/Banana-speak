ae789a839970963da0e546c25d0f12a37052a1cc
Priyallohar <128009939+Priyallohar@users.noreply.github.com>
2023-04-08 16:50:11 +0530

    call api and get output of our translation

--- app.js
+++ app.js
@@ -2,10 +2,20 @@ const btnTranslate = document.querySelector("#btn-translate")
 const inputtext = document.querySelector('#inputtext');
 const outputtext = document.querySelector("#outputtext")
 
+let URL = "https://api.funtranslations.com/translate/minion.json"
 
 
 function clickhandler(){
-    console.log("Clicked")
+    let modifiedURL = `${URL}?text=${inputtext.value}`
+    fetch(modifiedURL)
+      .then((response) => response.json())
+      .then((data) => (outputtext.innerHTML = data.contents.translated))
+      .catch(errorHandler)
 }
+
+function errorHandler(error) {
+    console.log('error occured', error.message)
+    alert('Please! Try after sometime')
+  }
 
 btnTranslate.addEventListener("click" , clickhandler)
