b9a7047c56c61d8e07e2fd9a96d0cc03de4d6bd0
Priyallohar <128009939+Priyallohar@users.noreply.github.com>
2023-04-08 10:50:15 +0530

    added text area and div for output

---
+++ app.js
@@ -0,0 +1,11 @@
+const btnTranslate = document.querySelector("#btn-translate")
+const inputtext = document.querySelector('#inputtext');
+const outputtext = document.querySelector("#outputtext")
+
+
+
+function clickhandler(){
+    console.log("Clicked")
+}
+
+btnTranslate.addEventListener("click" , clickhandler)
